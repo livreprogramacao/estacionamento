b339f79624e63406e4f8ba2a01b38128b767c0a3
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-01-17 10:54:38 -0200

    Update ModeloResources.java, MarcaResources.java

--- src/main/java/br/com/livreprogramacao/resources/marca/MarcaResources.java
+++ src/main/java/br/com/livreprogramacao/resources/marca/MarcaResources.java
@@ -1,25 +1,30 @@
 package br.com.livreprogramacao.resources.marca;
 
-import br.com.livreprogramacao.entity.modelo.Modelo;
+import br.com.livreprogramacao.entity.marca.Marca;
 import br.com.livreprogramacao.estacionamento.service.EstacionamentoService;
 import java.util.List;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author user
  */
 @Path("marcas")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MarcaResources {
     
     @Inject
     EstacionamentoService service;
 
     @GET
-    public List<Modelo> getModelos() {
-        return service.getModelos();
+    public List<Marca> getMarcas() {
+        return service.getMarcas();
     }
 
 }
--- src/main/java/br/com/livreprogramacao/resources/modelo/ModeloResources.java
+++ src/main/java/br/com/livreprogramacao/resources/modelo/ModeloResources.java
@@ -4,14 +4,19 @@ import br.com.livreprogramacao.entity.modelo.Modelo;
 import br.com.livreprogramacao.estacionamento.service.EstacionamentoService;
 import java.util.List;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author user
  */
 @Path("modelos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ModeloResources {
     
     @Inject
