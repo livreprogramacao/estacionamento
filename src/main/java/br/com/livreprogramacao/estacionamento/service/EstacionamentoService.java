63ae451642bfc1e6d2fb7fe8f381e1b76687051a
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-01-17 09:06:47 -0200

    Add calcularValor() method.

--- src/main/java/br/com/livreprogramacao/estacionamento/service/EstacionamentoService.java
+++ src/main/java/br/com/livreprogramacao/estacionamento/service/EstacionamentoService.java
@@ -1,10 +1,15 @@
 package br.com.livreprogramacao.estacionamento.service;
 
+import br.com.livreprogramacao.calculadora.CalculadoraMarcaEstacionamento;
+import br.com.livreprogramacao.calculadora.CalculadoraMarcaModeloEstacionamento;
+import br.com.livreprogramacao.calculadora.CalculadoraOutrosEstacionamento;
+import br.com.livreprogramacao.calculadora.CalculadoraPlacaB99Estacionamento;
 import br.com.livreprogramacao.entity.marca.Marca;
 import br.com.livreprogramacao.entity.modelo.Modelo;
 import br.com.livreprogramacao.entity.ticket.Ticket;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.Valid;
 
 public class EstacionamentoService {
 
@@ -54,4 +59,18 @@ public class EstacionamentoService {
     public void update(Ticket resource) {
         System.out.println("Resource Ticket updated!");
     }
+    
+    public void calcularValor(Ticket ticket) {
+        executarCalculadora(ticket);
+    }
+
+    private void executarCalculadora(@Valid Ticket ticket) {
+        Integer valor1 = new CalculadoraMarcaEstacionamento().resultado(ticket);
+        Integer valor2 = new CalculadoraMarcaModeloEstacionamento().resultado(ticket);
+        Integer valor3 = new CalculadoraPlacaB99Estacionamento().resultado(ticket);
+        Integer valor4 = new CalculadoraOutrosEstacionamento().resultado(ticket);
+        Integer valor = valor1+valor2+valor3+valor4;
+        System.out.format("Debug -- valor cobrado R$ %s para o Ticket %s", valor, ticket);
+    }
+    
 }
