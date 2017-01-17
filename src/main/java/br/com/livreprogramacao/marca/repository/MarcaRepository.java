91d121b59779573cd2816d707f8ea054259c71d9
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-01-17 11:47:40 -0200

    Add MarcaRepository.java

---
+++ src/main/java/br/com/livreprogramacao/marca/repository/MarcaRepository.java
@@ -0,0 +1,43 @@
+package br.com.livreprogramacao.marca.repository;
+
+import br.com.livreprogramacao.entity.marca.Marca;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author user
+ */
+public class MarcaRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public List<Marca> browse() {
+        final String sqlString = "select * from Marca m";
+        return em.createQuery(sqlString).getResultList();
+    }
+
+    public void create(final Marca marca) {
+        em.persist(marca);
+        em.flush();
+    }
+
+    public void update(final Marca marca) {
+        em.persist(marca);
+        em.flush();
+    }
+
+    public void delete(final Long marcaId) {
+        Marca marca = findById(marcaId);
+        if (marca != null) {
+            em.remove(marca);
+            em.flush();
+        }
+    }
+
+    private Marca findById(Long marcaId) {
+        return em.find(Marca.class, marcaId);
+    }
+}
